31338960857d5fe5c03c676de13396d5332b40f0
Guy <guyovadya12345@gmail.com>
2019-12-25 23:56:32 +0200

    Bird disappears

--- index/Bird.js
+++ index/Bird.js
@@ -1,11 +1,15 @@
+var verticalVelocity = 0;
+var gravity = 9.8;
+var isJumping = false;
 export default class Bird {
+  
   constructor (gameWidth, gameHeight) {
     this.width = 150;
     this.height = 100;
 
     this.position = {
 
-      x: gameWidth - this.width - 10,
+      x: gameWidth - this.width - (790-this.width),
 
       y: gameHeight / 2 - this.height / 2
 
@@ -15,5 +19,25 @@ export default class Bird {
   draw(ctx) {
     ctx.fillStyle = '#0000FF';
     ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+  update(dt) {
+    if(!dt) return;
+    this.position.x += 5/dt;
+    if (isJumping) {
+      yVel += gravity;
+      this.position.y += yVel;
+          if (this.position.y > characterGround) {
+              this.position.y = characterGround;
+              yVel = 0;
+              isJumping = false;
+             }
+      }
+  }
+  
+  jump() {
+    if(isJumping == false) {
+      verticalVelocity = -15;
+      isJumping = true;
+    }
   }
 }
--- index/index.js
+++ index/index.js
@@ -1,13 +1,35 @@
-import Bird from 'Bird.js';
+//imports
+import Bird from '/Bird.js';//importing bird
+import InputHandler from '/input.js';//importing the input handler
+//end of imports
 
+//canvas statements
 var canvas = document.getElementById("gameScreen");
 var ctx = canvas.getContext("2d");
+//end of canvas statements
 
-const GAME_WIDTH = 800;
-const GAME_HEIGHT = 600;
+//constant declarations
+const GAME_WIDTH = 800;//game width
+const GAME_HEIGHT = 600;//game height
+//end of constant declarations
 
-ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
 
 let bird = new Bird(GAME_WIDTH, GAME_HEIGHT);
 
-bird.draw(ctx);
+new InputHandler(bird);//handle inputs
+
+let lastTime = 0;
+
+function gameLoop(timeStamp) {
+    
+    let dt = timeStamp - lastTime;
+    lastTime = timeStamp;
+
+    ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+    bird.update(dt);
+    bird.draw(ctx);
+    
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
