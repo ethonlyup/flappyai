bccd3a7ce3e9443e27797c9cc4545d84fbbdb0d2
Ben <benabitbul@hotmail.com>
2019-12-30 02:34:20 +0200

    merge conflict

--- index/index.js
+++ index/index.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import Bird from '/Bird.js';
-
-var canvas = document.getElementById("gameScreen");
-var ctx = canvas.getContext("2d");
-
-const GAME_WIDTH = 800;
-const GAME_HEIGHT = 600;
-
-ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
-
-let bird = new Bird(GAME_WIDTH, GAME_HEIGHT);
-
-bird.draw(ctx);
-=======
 //imports
 import Bird from '/Bird.js';//importing bird
 import InputHandler from '/input.js';//importing the input handler
@@ -62,4 +47,3 @@ function gameLoop(timeStamp) {
 }
 
 gameLoop();
->>>>>>> 1467bef31ff8dd28bc26c45bba2f3e7254ea1dee
